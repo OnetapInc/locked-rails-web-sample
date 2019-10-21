668c6634e9973fd4ccef4602108eb8b5b7afd53f
kazukinakamuraKN <kajirikajiri@gmail.com>
2019-10-22 02:36:00 +0900

    fix takeda

--- app/helpers/encrypt_helper.rb
+++ app/helpers/encrypt_helper.rb
@@ -1,13 +1,7 @@
 module EncryptHelper
   def encrypt(unencrypted_email, secure_mode_salt)
-    key_len = ActiveSupport::MessageEncryptor.key_len('aes-256-cbc')
-    salt = SecureRandom.random_bytes(64)
-    key = ActiveSupport::KeyGenerator.new(secure_mode_salt).generate_key(secure_mode_salt + secure_mode_salt, key_len)
-    c = ::ActiveSupport::MessageEncryptor.new(key, cipher: 'aes-256-cbc')
-    e = c.encrypt_and_sign(unencrypted_email)
-    # eが暗号化した値、fが複合化した値
-    f = c.decrypt_and_verify(e)
-    binding.pry
+    encryptor = ::ActiveSupport::MessageEncryptor.new(secure_mode_salt, cipher: 'aes-256-cbc')
+    e = encryptor.encrypt_and_sign(unencrypted_email)
     return e
   end
 end
