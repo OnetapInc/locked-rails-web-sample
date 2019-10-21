f96648094a80bb465e580db5725e78847b2d61f5
kazukinakamuraKN <kajirikajiri@gmail.com>
2019-10-21 21:30:27 +0900

    一応できた

--- app/helpers/encrypt_helper.rb
+++ app/helpers/encrypt_helper.rb
@@ -1,11 +1,13 @@
 module EncryptHelper
   def encrypt(unencrypted_email, secure_mode_salt)
-    c = OpenSSL::Cipher.new("aes-256-cbc")
-    c.encrypt()
-    c.key = Digest::SHA256.digest(unencrypted_email)
+    key_len = ActiveSupport::MessageEncryptor.key_len('aes-256-cbc')
+    salt = SecureRandom.random_bytes(64)
+    key = ActiveSupport::KeyGenerator.new(secure_mode_salt).generate_key(secure_mode_salt + secure_mode_salt, key_len)
+    c = ::ActiveSupport::MessageEncryptor.new(key, cipher: 'aes-256-cbc')
+    e = c.encrypt_and_sign(unencrypted_email)
+    # eが暗号化した値、fが複合化した値
+    f = c.decrypt_and_verify(e)
     binding.pry
-    e = c.update(secure_mode_salt)
-    e << c.final
     return e
   end
 end
